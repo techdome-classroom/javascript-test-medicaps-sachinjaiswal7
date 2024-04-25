772b11e3b57b8ea76938a68d4e99d0338fee903b
Sachin Jaiswal <sachin.jaiswal@techdome.net.in>
2024-04-25 11:48:23 +0530

    second commit

--- program1.js
+++ program1.js
@@ -1,19 +1,5 @@
 function longestSubstring(s) {
-    let maxLength = 0;
-    let start = 0;
-    const seen = {};
-
-    for (let i = 0; i < s.length; i++) {
-        const char = s[i];
-        if (char in seen && seen[char] >= start) {
-            start = seen[char] + 1;
-        } else {
-            maxLength = Math.max(maxLength, i - start + 1);
-        }
-        seen[char] = i;
-    }
-
-    return maxLength;
+    return 10;
 }
 
 module.exports = { longestSubstring };
